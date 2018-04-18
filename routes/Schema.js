5e48c42a78aa0f6f33cc5d52f393cdf7e1214ed2
kmw <dai712@ajou.ac.kr>
2018-04-18 13:50:36 +0900

    start

---
+++ routes/Schema.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    Email : String,             //카카오 이메일
+    SavedNews : [String],       //저장한 뉴스
+    RecentNews : [String],      //최근 본 뉴스
+
+    Favorite : {
+        Category : [String],
+        Press : [String]        //즐겨찾는 분야와 언론사
+    }
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
