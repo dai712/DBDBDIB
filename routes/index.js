81be9b991b9a672e70cfede14a37ae5c05312089
kmw <dai712@ajou.ac.kr>
2018-04-30 15:13:47 +0900

    2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var client = require('cheerio-httpcli');
+var request = require('request');
+var fs = require('fs');
+var urlType = require('url');
+var savedir = __dirname + '/img';
+if(!fs.existsSync(savedir)){
+    fs.mkdirSync(savedir);
+}
 
-//db.dropDatabase();
+//URL 지정
+var url = 'https://ko.wikipedia.org/wiki/' + encodeURIComponent('강아지');
+var param = {};
+
+//html 파일 획득
+client.fetch(url, param, function(err, $, res){
+    if(err) {console.log(err); return;}
+    //img 링크 추출 후 함수 실행
+    $('img').each(function(idx){
+        var src = $(this).attr('src');
+        //상대 경로 -> 절대 경로
+        src = urlType.resolve(url, src);
+
+        //저장 파일 이름 설정
+        var fname = urlType.parse(src).pathname;
+        fname = savedir + '/' + fname.replace(/[^a-zA-Z0-9\.]+/g, '_');
+        //다운로드
+        request(src).pipe(fs.createWriteStream(fname));
+    });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -27,7 +54,7 @@ router.post('/message', (req, res) => {
         content: req.body.content
     };
 
-    const massage = {
+    const message = {
         "message": {
             "text": '',
         },
@@ -38,25 +65,25 @@ router.post('/message', (req, res) => {
 
     switch(_obj.content) {
         case '뉴스 보기' :
-            massage.message.text = '뉴스보기 실행';
-            massage.keyboard.type = 'buttons';
-            massage.keyboard.buttons = [
+            message.message.text = '뉴스보기 실행';
+            message.keyboard.type = 'buttons';
+            message.keyboard.buttons = [
                 "장르",
                 "언론사",
                 "키워드 검색",
             ];
             break;
         case '저장 목록' :
-            massage.message.text = '저장목록 실행';
+            message.message.text = '저장목록 실행';
             break;
         case '즐겨 찾기' :
-            massage.message.text = '즐겨찾기 실행';
+            message.message.text = '즐겨찾기 실행';
             break;
 
         case '장르' :
-            massage.message.text = '장르 선택';
-            massage.keyboard.type = 'buttons';
-            massage.keyboard.buttons = [
+            message.message.text = '장르 선택';
+            message.keyboard.type = 'buttons';
+            message.keyboard.buttons = [
                 "속보",
                 "정치",
                 "경제",
@@ -67,9 +94,9 @@ router.post('/message', (req, res) => {
             ];
             break;
         case '언론사' :
-            massage.message.text = '언론사 선택';
-            massage.keyboard.type = 'buttons';
-            massage.keyboard.buttons = [
+            message.message.text = '언론사 선택';
+            message.keyboard.type = 'buttons';
+            message.keyboard.buttons = [
                 "경향",
                 "국민",
                 "동아",
@@ -82,13 +109,16 @@ router.post('/message', (req, res) => {
                 "한국",
             ];
             break;
+        case '경향' :
+            message.message.text = '테스트';
+            break;
         case '키워드 검색' :
-            massage.message.text = '입력해 주세용';
-            massage.keyboard.type = 'text';
+            message.message.text = '입력해 주세용';
+            message.keyboard.type = 'text';
             break;
     }
 
-res.set({'content-type': 'application/json'}).send(JSON.stringify(massage));
+res.set({'content-type': 'application/json'}).send(JSON.stringify(message));
 });
 
 
