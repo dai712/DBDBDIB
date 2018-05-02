c345ee5e328718e972e83112431fad384db8a570
kmw <dai712@ajou.ac.kr>
2018-05-02 14:03:00 +0900

    3

--- routes/index.js
+++ routes/index.js
@@ -37,11 +37,14 @@ request(url, function(err, res, body){
    if(err) console.log(err);
    //console.log('body : ', body);
 
-   var $ = cheerio.load(body);
+    var $ = cheerio.load(body)
+
+    var test = $('#main_content > div.list_body.newsflash_body > ul.type06_headline > li:nth-child(1) > dl > dt:nth-child(2)');
 
     let json = [], title;
-    $("#main_content > div > div._persist > div:nth-child(1) > div:nth-child(1) > div.cluster_body > ul > li:nth-child(1)").each(function(index, ele){ // <div class="recomm_blog">를 반복
-        title = $(this).find('a > .cluster_text_headline nclicks(cls_nav.clsart)').text() // a태그 아래에 class="tit_subject"의 텍스트(제목)
+
+    $('#main_content > div > div._persist > div:nth-child(1) > div:nth-child(1) > div.cluster_body > ul > li:nth-child(1)').each(function(index, ele){ // <div class="recomm_blog">를 반복
+        title = $(this).find('a').text() // a태그 아래에 class="tit_subject"의 텍스트(제목)
         json.push({ title: title})
     });
 
