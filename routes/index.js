54a350359d4c194dbf362f56470a988ec6da7141
kmw <dai712@ajou.ac.kr>
2018-05-12 00:58:34 +0900

    1212

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,29 @@ var savedir = __dirname + '/img';
 if(!fs.existsSync(savedir)){
     fs.mkdirSync(savedir);
 }
-
+var client_id = 'uD_8GWD3pP_KXJJRKecZ';
+var client_secret = '7OTLr047fX';
+app.get('/search/blog', function (req, res) {
+    var api_url = 'https://openapi.naver.com/v1/search/news?query=' + encodeURI(req.query.query); // json 결과
+//   var api_url = 'https://openapi.naver.com/v1/search/blog.xml?query=' + encodeURI(req.query.query); // xml 결과
+    var request = require('request');
+    var options = {
+        url: api_url,
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+    request.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+});
+app.listen(3000, function () {
+    console.log('http://127.0.0.1:3000/search/blog?query=검색어 app listening on port 3000!');
+});
 /*
 //URL 지정
 var url = 'https://ko.wikipedia.org/wiki/' + encodeURIComponent('강아지');
@@ -32,7 +54,7 @@ client.fetch(url, param, function(err, $, res){
         request(src).pipe(fs.createWriteStream(fname));
     });
 });
-*/
+
 url = 'http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=102';
 request(url, function(err, res, body){
    if(err) console.log(err);
@@ -45,13 +67,13 @@ request(url, function(err, res, body){
     let json = [], title;
 
     $('#main_content > div > div._persist > div:nth-child(1) > div:nth-child(1) > div.cluster_body > ul > li:nth-child(1)').each(function(index, ele){ // <div class="recomm_blog">를 반복
-        title = $(this).find('a').text() // a태그 아래에 class="tit_subject"의 텍스트(제목)
+        title = $(this).find('a').text(); // a태그 아래에 class="tit_subject"의 텍스트(제목)
         json.push({ title: title})
     });
 
     console.log('json: ', json);
 });
-
+*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
