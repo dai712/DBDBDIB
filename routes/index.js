9febf5a0158c6cd8fd6b135f69fc5c1f6ddbf27e
kmw <dai712@ajou.ac.kr>
2018-05-12 01:11:06 +0900

    clear1

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,27 @@ var savedir = __dirname + '/img';
 if(!fs.existsSync(savedir)){
     fs.mkdirSync(savedir);
 }
+
+var client_id = 'uD_8GWD3pP_KXJJRKecZ';
+var client_secret = '7OTLr047fX';
+router.get('/search/blog', function (req, res) {
+    var api_url = 'https://openapi.naver.com/v1/search/news?query=' + encodeURI(req.query.query); // json 결과
+//   var api_url = 'https://openapi.naver.com/v1/search/blog.xml?query=' + encodeURI(req.query.query); // xml 결과
+    var request = require('request');
+    var options = {
+        url: api_url,
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+    request.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+});
 /*
 //URL 지정
 var url = 'https://ko.wikipedia.org/wiki/' + encodeURIComponent('강아지');
