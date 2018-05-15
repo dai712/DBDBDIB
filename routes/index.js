b9caf8be683dc4d1b44c18a247b67dd7efc69149
kmw <dai712@ajou.ac.kr>
2018-05-15 19:32:57 +0900

    qwfqwf

--- routes/index.js
+++ routes/index.js
@@ -215,9 +215,9 @@ router.post('/message', (req, res) => {
                     }*/
 
                    while(true){
-                       if(titles[0] !== ''){
+                       if(titles[0] !== undefined){
                            console.log(titles[0]);
-                           
+
                            message1.keyboard.buttons = [
                                "(" + field + ")" +  titles[0],
                                "(" + field + ")" +  titles[1],
