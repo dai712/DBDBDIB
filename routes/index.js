159d69b90f95aa86e6a114e0dcec47de84528daa
kmw <dai712@ajou.ac.kr>
2018-05-18 14:37:19 +0900

    fqwf

--- routes/index.js
+++ routes/index.js
@@ -239,7 +239,7 @@ function getSavedNews(user_key) {
            console.log(tempNews);
            console.log(tempNews.length);
            for(i=0 ; i<tempNews.length ; i++) {
-               FieldNews.findOne({_id : ObjectId(tempNews[i])}, function(err, retDoc){
+               FieldNews.findOne({_id : tempNews[i]}, function(err, retDoc){
                    console.log('시발');
                    if(err) console.log(err);
                    else if(retDoc !== null) {
@@ -249,7 +249,7 @@ function getSavedNews(user_key) {
                        console.log('못찾음.(필드)');
                    }
                });
-               PressNews.findOne({_id: ObjectId(tempNews[i])}, function(err, retDoc){
+               PressNews.findOne({_id: tempNews[i]}, function(err, retDoc){
                    if(err) console.log(err);
                    else if(retDoc !== null) {
                        savedTitles.push(retDoc.Title);
