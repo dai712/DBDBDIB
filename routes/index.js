70d707044965f70244b99a353574b24a3eea3836
kmw <dai712@ajou.ac.kr>
2018-06-09 19:02:04 +0900

    fqfw

--- routes/index.js
+++ routes/index.js
@@ -105,12 +105,9 @@ function crawlingNews(targetURL, selector1, selector2, imgSelector1, imgSelector
                 var crawSelector = selector1 + i + selector2;
                 var crawImgSelector = imgSelector1 + i + imgSelector2;
                 $(crawSelector).each(function(index, value){
-                    if(targetURL === fieldURLs[0] || selector1 === pressSelector1){         //속보일때 title, url
                         //var title = $(this).find('a').text().replace( /(\s*)/g, "");
                         var title = $(this).find('a').text().trim();
-                    } else {
-                        var title = $(this).find('a').text();                       //나머지 분야.
-                    }
+
                     var url = $(value).find('a').attr('href');
                     titles.push(title);
                     urls.push(url);
