ce4da0b9424ac14a3ec42cc7050237e3069c783f
kmw <dai712@ajou.ac.kr>
2018-06-10 18:59:38 +0900

    fqfw

--- routes/index.js
+++ routes/index.js
@@ -163,6 +163,7 @@ function crawling(k){
                     FieldNews.findOne({'Title': titles[p]}, {new: true}, function (err, doc) {
                         if (err) console.log(err);
                         if (doc === null) {
+                            console.log(titles[p]);
                             var updateFieldNews = new FieldNews();
                             updateFieldNews.Title = titles[p];
                             updateFieldNews.Url = urls[p];
