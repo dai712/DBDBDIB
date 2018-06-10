70e47ae1adbe476459a4548c52bce54b173c7477
kmw <dai712@ajou.ac.kr>
2018-06-10 16:59:16 +0900

    fqfw

--- routes/index.js
+++ routes/index.js
@@ -163,7 +163,7 @@ function crawling(k){
                     console.log('분야');
                     crawSelector = fieldSelector1 + i + fieldSelector2;
                     crawImgSelector = fieldImgSelector1 + i + fieldImgSelector2;
-                } else if (k === 0){
+                } else{
                     console.log('속보');
                     crawSelector = breakingSelector1 + i + breakingSelector2;
                     crawImgSelector = breakingImgSelector1 + i + breakingImgSelector2;
