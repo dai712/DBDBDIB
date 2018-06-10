d342cf2e81dfc3cd966f401978a2a4c7e1590f5d
kmw <dai712@ajou.ac.kr>
2018-06-10 23:39:46 +0900

    fqfw

--- routes/index.js
+++ routes/index.js
@@ -635,7 +635,7 @@ setTimeout(function() {
                            setTimeout(function(){
                                returnNews = doc;
                                console.log(returnNews);
-                           },1000);
+                           },2000);
 
                        });
                    } else {
@@ -643,7 +643,7 @@ setTimeout(function() {
                        setTimeout(function(){
                            returnNews = doc;
                            console.log(returnNews);
-                       },1000);
+                       },2000);
                    }
                 });
 
